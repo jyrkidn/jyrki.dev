4cca71e72a39ca52b276fd9d970868eba24dfdcf
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-02-18 15:50:32 +0200

    Start with basic styling

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="absolute top-2 right-0 left-0 z-40 pt-14">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:max-w-none">
+                <div class="flex items-center justify-between">
+                    <a aria-label="Home" href="/">
+                        <x-logo />
+                    </a>
+                    <div class="flex items-center gap-x-8">
+                        <a class="inline-flex rounded-full bg-purple-600 px-4 py-1.5 text-sm font-semibold text-white transition hover:bg-purple-400" href="/contact"><span class="relative top-px">Contact us</span></a
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
